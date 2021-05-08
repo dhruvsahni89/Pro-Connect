3fd44f8a7959e499fa8d0ff11ac4738368aeeb02
Dhruv Sahni <dhruvsahni89@gmail.com>
2021-05-08 15:30:30 +0530

    adding navbar componenets at home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,20 +14,67 @@ const Header=(props)=>{
                             <input type="text" placeholder="Search" />
                         </div>
                         <SearchIcon>
-                        <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              data-supported-dps="16x16"
-              fill="currentColor"
-              width="16"
-              height="16"
-              focusable="false"
-            >
-              <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
-            </svg>
+                            <img src="/images/search-icon.svg" alt="" />
+                        
                         </SearchIcon>
 
                     </Search>
+                    <Nav>
+                        <NavListWrap>
+                            <NavList className="active">
+                               <a>
+                                   <img src="/images/nav-home.svg" alt="" />
+                                   <span>Home</span>
+                               </a>
+                            </NavList>
+
+                            <NavList>
+                               <a>
+                                   <img src="/images/nav-network.svg" alt="" />
+                                   <span>My Network</span>
+                               </a>
+                            </NavList>
+
+                            <NavList>
+                               <a>
+                                   <img src="/images/nav-jobs.svg" alt="" />
+                                   <span>Jobs</span>
+                               </a>
+                            </NavList>
+
+                            <NavList>
+                               <a>
+                                   <img src="/images/nav-messaging.svg" alt="" />
+                                   <span>Messaging</span>
+                               </a>
+                            </NavList>
+
+                            <NavList>
+                               <a>
+                                   <img src="/images/nav-notifications.svg" alt="" />
+                                   <span>Notifications</span>
+                               </a>
+                            </NavList>
+                            <User>
+                                <a>
+                                    <img src="/images/user.svg" alt="" />
+                                    <span>Me</span>
+                                    <img src="/images/down-icon.svg" alt="" />
+
+                                </a>
+                                <SignOut>
+                                    <a>Sign Out</a>
+                                </SignOut>
+                            </User>
+                            <Work>
+                                <a>
+                                <img src="/images/nav-work.svg" alt="" />
+                                <span>Work   <img src="/images/down-icon.svg" alt="" /></span>
+                                </a>
+                            </Work>
+
+                        </NavListWrap>
+                    </Nav>
             </Content>
         </Container>
     );
@@ -100,8 +147,132 @@ left:2px;
 border-radius:0 2px 2px 0;
 margin:0;
 pointer-events:none;
+display:flex;
+justify-content:center;
+align-items:center;
+
+
 
 `;
+const Nav=styled.nav`
+margin-left:auto;
+display:block;
+@media (max-width:768px){
+    position:fixed;
+    left:0;
+    bottom:0;
+    background:white;
+    width:100%;
+
+
+}
+`;
+const NavListWrap=styled.ul`
+display:flex;
+flex-wrap:nowrap;
+list-style-type:none;
+.active{
+    span:after{
+        content: "";
+        transform:scaleX(1);
+        border-bottom:2px solid  var( --white, #fff);
+        bottom:0;
+        left:0;
+        position:absolute;
+        transition: transform 0.2s ease-in-out;
+        width:100%;
+        border-color:rgba(0,0,0,0.9);
+
+    }
+}
+`;
+const NavList=styled.li`
+display:flex;
+align-items:center;
+a{
+    align-items:center;
+    background:transparent;
+    display:flex;
+    flex-direction:column;
+    font-size:12px;
+    font-weight:400;
+    justify-content:center;
+    line-height:1.5;
+    min-height:52px;
+    min-width:80px;
+    position:relative;
+    text-decoration:none;
+    span{
+        color:rgba(0,0,0,0.6);
+        display:flex;
+        align-items:center;
+
+
+    }
+    @media (max-width:768px){
+        min-width:70px;
+
+    }
+}
+&:hover, &:active{
+    a{
+        span{
+            color:rgba(0,0,0,0.9);
+        }
+    }
+}
+`;
+const SignOut=styled.div`
+position:absolute;
+top:45px;
+background:white;
+border-radius:0 0 5px 5px;
+width:100px;
+height:40px;
+font-size:16px;
+transition-duration:167ms;
+text-align:center;
+display:none;
+
+`;
+
+const User=styled(NavList)`
+a> svg{
+    width:24px;
+    border-radius:50%;
+
+}
+a>img{
+    width:24px;
+    height:24px;
+    border-radius:50%;
+
+}
+span{
+    display:flex;
+    align-items:center;
+
+}
+
+&:hover{
+    ${SignOut}{
+        align-items:center;
+        display:flex;
+        justify-content:center;
+
+    }
+
+}
+`;
+const Work=styled(User)`
+border-left:1px solid rgba(0,0,0,0.08);
+
+`;
+
+
+
+
+
 
 
 
