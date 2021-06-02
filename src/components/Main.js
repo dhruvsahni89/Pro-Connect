9c6c22282a3b6192bc355554b3fd3484c8435f38
Dhruv Sahni <dhruvsahni89@gmail.com>
2021-06-02 12:19:27 +0530

    making feedbox and rightside completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,80 @@
 import styled from 'styled-components';
 
 const Main=(props)=>{
-    return <Container>Main</Container>
+    return <Container>
+        <ShareBox>Share
+        <div>
+            <img src="/images/user.svg" alt="" />
+            <button>start a post</button>
+        </div>
+        <div><button>
+            <img src="/images/feed-icon.svg" alt="" />
+            <span>Photo</span>
+            </button>
+            <button>
+                <img src="/images/feed-icon.svg" alt="" />
+                <span>Video</span>
+            </button>
+            <button>
+                <img src="/images/feed-icon.svg" alt="" />
+                <span>event</span>
+            </button>
+            <button>
+                <img src="/images/feed-icon.svg" alt="" />
+                <span>Write Article</span>
+            </button>
+
+            </div>
+            </ShareBox>
+    </Container>
 
     
 }
 const Container=styled.div`
 grid-area:Main;
 `;
+const CommonCard=styled.div`
+text-align:center;
+overflow:hidden;
+margin-bottom: 8px;
+background-color: #fff;
+border-radius: 5px;
+position: relative;
+border:none;
+box-shadow: 0 0 0 1px rgb(0 0 0 /15%),0 0 0 rgb(0 0 0/20%);
+
+
+`;
+const ShareBox=styled(CommonCard)`
+display:flex;
+flex-direction:column;
+color:#958b7b;
+margin:0 0 8px;
+background:white;
+div{
+    button{
+        outline:none;
+        color:rgba(0,0,0,0.6);
+        font-size: 14px;
+        line-height: 1.5;
+        min-height:48px;
+        background:transparent;
+            border:none;
+            display:flex;
+            align-items:center;
+            font-weight:600;
+    }
+    &:first-child{
+    display:flex;
+    align-items:center;
+    padding:8px 16px 0px 16px;
+    img{
+        width:48px;
+    }
+    }
+}
+`;
+
+
 export default Main;
 
--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -26,7 +26,14 @@ const Rightside=(props)=>{
                 </div>
             </li>
         </FeedList>
-        </FollowCard></Container>
+        <Recommendation>view all recommendations
+        <img src="/images/right-icon.svg" alt="" />
+        </Recommendation>
+        </FollowCard>
+        <BannerCard>
+            <img src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg" alt="" />
+        </BannerCard>
+        </Container>
 
     
 }
@@ -66,11 +73,52 @@ font-size:14px;
     display:flex;
     flex-direction:column;
 }
+button{
+    background-color:transparent;
+    color:rgba(0,0,0,0.6);
+    box-shadow:inset 0 0 0 1px rgba(0,0,0,0.6);
+    padding:16px;
+    align-items:center;
+    border-radius:15px;
+    box-sizing:border-box;
+    font-weight:600;
+    display:inline-flex;
+    justify-content:center;
+    max-height:32px;
+    max-width:480px;
+    text-align:center;
+    outline:none;
+}
 }
 
 `;
-const Avatar=styled.div``;
+const Avatar=styled.div`
+background-image: url("https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs");
+background-size:contain;
+background-position:center;
+background-repeat:no-repeat;
+width:48px;
+height:48px;
+margin-right:8px;
 
+`;
+const Recommendation=styled.a`
+color:#0a66c2;
+display:flex;
+align-items: center;
+font-size: 14px;
+
+
+`;
+
+const BannerCard=styled(FollowCard)`
+img{
+    width:100%;
+    height:100%;
+    
+}
+
+`;
 
 
 export default Rightside;
