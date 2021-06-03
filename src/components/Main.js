f4496f17382815fc9d7c8aa20554eb08edd3dae7
Dhruv Sahni <dhruvsahni89@gmail.com>
2021-06-03 15:50:34 +0530

    added feedbox and like comment display

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,11 @@ const Main=(props)=>{
             <button>start a post</button>
         </div>
         <div><button>
-            <img src="/images/feed-icon.svg" alt="" />
+            <img src="/images/photo.svg" alt="" />
             <span>Photo</span>
             </button>
             <button>
-                <img src="/images/feed-icon.svg" alt="" />
+                <img src="/images/photo.svg" alt="" />
                 <span>Video</span>
             </button>
             <button>
@@ -26,6 +26,45 @@ const Main=(props)=>{
 
             </div>
             </ShareBox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src="/images/user.svg" alt="" />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src="/images/right-icon.svg" alt="" />
+                        </button>
+                    </SharedActor>
+                    <Description>Description</Description>
+                    <SharedImg>
+                        <a>
+                            <img src="/images/sharedimage.webp" alt="" />
+                        </a>
+                    </SharedImg>
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" alt="" />
+                                <img src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" alt="" />
+                                <span>75</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>2 comments</a>
+                        </li>
+                    </SocialCounts>
+                    <button>
+                        <img src="/images/plus-icon.svg" alt="" />
+                        <span>Like</span>
+                    </button>
+                </Article>
+            </div>
     </Container>
 
     
@@ -70,11 +109,132 @@ div{
     padding:8px 16px 0px 16px;
     img{
         width:48px;
+        border-radius:50%;
+        margin-right:8px;
     }
+    button{
+        margin:4px 0;
+        flex-grow:1;
+        border-radius:35px;
+        padding-left:16px;
+        border:1px solid rgba(0,0,0,0.15);
+        border-radius:35px;
+        background-color:white;
+        text-align:center;
+
+    }
+    }
+    &:nth-child(2){
+        display:flex;
+        flex-wrap:wrap;
+        justify-content: space-around;
+        padding-bottom: 4px;
+        button{
+            img{
+            margin:0 4px 0 -2px;
+            }
+            span{
+                color:#70b5f9;
+            }
+        }
     }
 }
 `;
+const Article=styled(CommonCard)`
+padding:0;
+margin:0 0 8px;
+overflow:visible;
+`;
+const SharedActor=styled.div`
+padding-right:40px;
+flex-wrap:nowrap;
+padding:12px 16px 0;
+margin-bottom:8px;
+align-items:center;
+display:flex;
+a{
+    margin-right:12px;
+    flex-grow:1;
+    overflow:hidden;
+    display:flex;
+    text-decoration:none;
+    img{
+        width:48px;
+        height:48px;
 
+    }
+    &>div{
+        display:flex;
+        flex-direction: column;
+        flex-grow:1;
+        flex-basis:0;
+        margin-left:8px;
+        overflow:hidden;
+        span{
+            text-align:left;
+            &:first-child{
+                font-size:14px;
+                font-weight:700;
+                color:rgba(0,0,0,1);
+
+            }
+            &:nth-child(n+1){
+                font-size:12px;
+                color:rgba(0,0,0,0.6);
+            }
+        }
+    }
+}
+
+button{
+    position:absolute;
+    right:12px;
+    top:0;
+    background:transparent;
+    border:none;
+    outline:none;
+
+}
+`;
+const Description=styled.div`
+padding:0 16px;
+overflow:hidden;
+color:rgba(0,0,0,0.9);
+font-size:14px;
+text-align:left;
+`;
+const SharedImg=styled.div`
+margin-top:8px;
+width:100%;
+display:block;
+position:relative;
+background-color: #f9fafb;
+img{
+    object-fit:contain;
+    width:100%;
+    height:100%;
+
+}
+`;
+
+const SocialCounts=styled.ul`
+line-height:1.3;
+display:flex;
+align-items:flex-start;
+overflow:auto;
+margin:0 16px;
+padding:8px 0;
+border-bottom:1px solid #e9e5df;
+list-style:none;
+li{
+    margin-right:5px;
+    font-size:12px;
+    button{
+        display:flex;
+    }
+}
+
+`;
 
 export default Main;
 
